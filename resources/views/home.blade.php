22d84e9703ee844131d3a04eb8e5396c3e265cc8
trungvo4 <125561854+trungvo4@users.noreply.github.com>
2024-07-12 01:49:39 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Đề xuất thực đơn</title>
-
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.ico') }}">
     <!-- Custom Stylesheet -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-home.css') }}">
 
 </head>
 
@@ -26,15 +23,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-5 text-center mt-lg-5 content-text">
-                        <h1 class="text-white">THỰC ĐƠN DÀNH CHO BẠN</h1>
-                        <p class="fw-500 text-grey-500 pt-2 pb-2">Bạn đang muốn thay đổi cân nặng bản thân, bạn đang
+                        <h1 class="text-green">THỰC ĐƠN DÀNH CHO BẠN</h1>
+                        <p class="fw-500 text-grey-500 pt-2 pb-2 text-green">Bạn đang muốn thay đổi cân nặng bản thân,
+                            bạn đang
                             muốn tìm kiếm thực đơn phù hợp cho mình gia đình của bạn</p>
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <a href="{{ route('login') }}"
-                                class="bg-success btn btn-lg p-3 rounded-6 font-xsssss fw-700 ls-3 text-white w-175 text-uppercase">Bắt
-                                đầu nào!</a>
-                        </form>
+                        <button onclick="showUserTypeModal()"
+                            class="bg-success btn btn-lg p-3 rounded-6 font-xsssss fw-700 ls-3 text-white w-175 text-uppercase">Bắt
+                            đầu nào!</button>
                     </div>
                 </div>
             </div>
@@ -43,8 +38,76 @@
         @include('partialsHome.footer')
     </div>
 
+    <!-- Modal -->
+    <div id="userTypeModal" class="modal" style="display:none;">
+        <div class="modal-content">
+            <span class="close" onclick="closeUserTypeModal()">&times;</span>
+            <h2>Bạn là ai?</h2>
+            <button onclick="redirectTo('{{ route('login.index') }}')" class="btn btn-primary">Khách hàng</button>
+            <button onclick="redirectTo('{{ route('adminLogin.index') }}')" class="btn btn-primary">Quản lý</button>
+        </div>
+    </div>
+
     <script src="{{ asset('js/plugin.js') }}"></script>
     <script src="{{ asset('js/scripts.js') }}"></script>
+    <script>
+        function showUserTypeModal() {
+            document.getElementById('userTypeModal').style.display = 'block';
+        }
+
+        function closeUserTypeModal() {
+            document.getElementById('userTypeModal').style.display = 'none';
+        }
+
+        function redirectTo(route) {
+            window.location.href = route;
+        }
+    </script>
+
+    <style>
+        /* Modal styles */
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgb(0, 0, 0);
+            background-color: rgba(0, 0, 0, 0.4);
+            padding-top: 60px;
+        }
+
+        .modal-content {
+            background-color: #fefefe;
+            margin: 5% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+            max-width: 500px;
+            text-align: center;
+        }
+
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        .btn {
+            margin: 10px;
+        }
+    </style>
 
 </body>
 
